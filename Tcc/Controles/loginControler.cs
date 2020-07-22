191cf1d18c606a6cbd1ab6c5e4fc969865b69db9
JadirJunior <jadirjunior8@gmail.com>
2020-07-22 11:42:21 -0300

    Controles para comunicação entre as classes para o banco e os views

---
+++ Tcc/Controles/loginControler.cs
@@ -0,0 +1,67 @@
+﻿using PrjTreino.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrjTreino.Controles
+{
+    public class loginControler
+    {
+        public bool resultado;
+        public bool Erro = false;
+        LoginComands login = new LoginComands();
+
+        public String iniciarSessao(String Usuario, String senha)
+        {
+
+            resultado = login.iniciarSessao(Usuario, senha);
+            if (login.Erro) { Erro = true; return "Ocorreu algum erro, tente novamente";  }
+                
+            if (resultado) { Erro = false; return "Bem vindo"; }
+
+            return "Usuário ou senha incorretos";
+        }
+
+        public bool cadastrarFuncionario(String Usuario, String senha)
+        {
+
+            resultado = login.CadastrarFuncionario(Usuario, senha);
+            if (login.Erro) { Erro = true; return false; }
+
+
+            if (resultado) { Erro = false; return true; }
+
+            if (login.Erro ==false && resultado == false) { return false; }
+
+            return false;
+        }
+
+        public bool atualizarFuncionario(String Usuario, String Senha)
+        {
+            resultado = login.atualizarUsuario(Usuario, Senha);
+
+            if (login.Erro) { Erro = true; return false; }
+
+            if (resultado) { Erro = false; return true; }
+
+            if (login.Erro == false && resultado ==false) { return false; }
+
+            return false;
+        }
+
+        public bool atualizarFuncionarioComImagem(String Usuario, String Senha, String Caminho)
+        {
+            resultado = login.atualizarUsuarioComImagem(Usuario, Senha, Caminho);
+
+            if (login.Erro) { Erro = true; return false; }
+
+            if (resultado) { Erro = false; return true; }
+
+            if (login.Erro == false && resultado == false) { return false; }
+
+            return false;
+        }
+    }
+}
