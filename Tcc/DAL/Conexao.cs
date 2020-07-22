a9cb81d1b790453b941b34f9197eb894ad3d0dcf
JadirJunior <jadirjunior8@gmail.com>
2020-07-22 11:41:29 -0300

    Classes para comunicar-se com banco de dados

---
+++ Tcc/DAL/Conexao.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrjTreino.DAL
+{
+    public class Conexao
+    {
+        SqlConnection con = new SqlConnection();
+
+        public Conexao()
+        {
+            con.ConnectionString = @"Initial Catalog = PrjTCCteste; Integrated Security = TRUE; Data Source="+Environment.MachineName;
+        }
+
+        public SqlConnection Conectar()
+        {
+            if (con.State.Equals(System.Data.ConnectionState.Closed))
+                con.Open();
+
+            return con;
+        }
+
+        public void Desconectar()
+        {
+            if (con.State.Equals(System.Data.ConnectionState.Open))
+                con.Close();
+        }
+
+    }
+}
