e190516b2be4deb74c731f26d3fbce61cdd6dacd
JadirJunior <jadirjunior8@gmail.com>
2020-10-03 01:51:53 -0300

    Código da View Da Box De adicionar Postagem

---
+++ Tcc/PrjTreino/View/PostagemBox.xaml.cs
@@ -0,0 +1,47 @@
+﻿using PrjTreino.Controles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PrjTreino.View
+{
+    /// <summary>
+    /// Lógica interna para PostagemBox.xaml
+    /// </summary>
+    public partial class PostagemBox : Window
+    {
+        public PostagemBox()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            new PostagemController().addPostagem(new DAL.models.Postagem()
+            {
+                AuthorId = StaticKeys.id_func,
+                AuthorName = StaticKeys.Usuario,
+                Content = txtPostagem.Text
+            });
+
+            txtPostagem.Text = "";
+
+            Mural.renderPosts();
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
