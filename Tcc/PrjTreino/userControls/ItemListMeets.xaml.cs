343d58b4ba54d10d02aecf804649e4b6db3f88e6
JadirJunior <jadirjunior8@gmail.com>
2020-10-03 01:25:16 -0300

    Código do User Control de cada item da lista de reuniões

---
+++ Tcc/PrjTreino/userControls/ItemListMeets.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrjTreino.userControls
+{
+    /// <summary>
+    /// Interação lógica para ItemListMeets.xam
+    /// </summary>
+    public partial class ItemListMeets : UserControl
+    {
+        public ItemListMeets(String text)
+        {
+            InitializeComponent();
+            MeetDescription.Text = text;
+        }
+    }
+}
