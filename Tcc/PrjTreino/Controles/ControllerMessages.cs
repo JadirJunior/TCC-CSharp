d7129a06e2ddceaba1662cee3d1c823e678e2edc
JadirJunior <jadirjunior8@gmail.com>
2020-10-03 01:47:07 -0300

    Controller para comunicação com a classe que conecta diretamente ao banco

---
+++ Tcc/PrjTreino/Controles/ControllerMessages.cs
@@ -0,0 +1,21 @@
+﻿using PrjTreino.DAL;
+using PrjTreino.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrjTreino.Controles
+{
+    public class ControllerMessages
+    {
+
+        public static List<Message> getMessages()
+        {
+            MessagesCommand messages = new MessagesCommand();
+
+            return messages.carregarMensagens();
+        }
+    }
+}
