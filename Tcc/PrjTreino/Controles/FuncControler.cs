aff4e8f57da7ad74245bc77b301338b666d1f19c
JadirJunior <jadirjunior8@gmail.com>
2020-10-03 01:47:21 -0300

    Controller para comunicação com a classe que conecta diretamente ao banco

---
+++ Tcc/PrjTreino/Controles/FuncControler.cs
@@ -0,0 +1,38 @@
+﻿using PrjTreino.DAL;
+using PrjTreino.DAL.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrjTreino.Controles
+{
+    public class FuncControler
+    {
+        FuncCommands commands = new FuncCommands();
+        private List<Funcionario> funcs;
+
+        public Funcionario Funcionario
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public FuncCommands FuncCommands
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public List<Funcionario> selectAllFuncs()
+        {
+            funcs = commands.selectAllFuncs();
+            return funcs;
+        }
+    }
+}
