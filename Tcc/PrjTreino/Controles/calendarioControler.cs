1787cfd27bdc6c2f701792eb8bad7ccfb301ea85
JadirJunior <jadirjunior8@gmail.com>
2020-10-03 01:46:53 -0300

    Controller para comunicação com a classe que conecta diretamente ao banco

---
+++ Tcc/PrjTreino/Controles/calendarioControler.cs
@@ -0,0 +1,35 @@
+﻿using PrjTreino.DAL;
+using PrjTreino.userControls.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrjTreino.Controles
+{
+   public class calendarioControler
+    {
+        public bool Erro = false;
+        calendarioComandos calendarioComandos;
+        public bool selecionarCalendario(int id_func)
+        {
+            calendarioComandos = new calendarioComandos();
+            calendarioComandos.selecionarCompromissos(id_func);
+            if (calendarioComandos.Erro)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool cadastrarCompromisso(Compromisso compromisso)
+        {
+            calendarioComandos = new calendarioComandos();
+            calendarioComandos.cadastrarData(compromisso);
+            if (calendarioComandos.Erro)
+                return false;
+            return true;
+        }
+    }
+}
